09fd01eba4becd3193bcf9697e524e5226b5f0e8
Admin <jmssri31@gmail.com>
2018-08-08 16:10:30 +0530

    initial

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'scenario1';
+ private text="";
+ 
+ OnClickMe(text) {
+     console.log(text);
+   }
+
+  
+  
+}
